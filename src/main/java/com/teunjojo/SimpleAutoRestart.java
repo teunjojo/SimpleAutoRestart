93602c609ef6009ceada11baf216351056426491
Teuntje Kuipers <mail@teunjojo.com>
2023-11-11 01:07:55 +0100

    #2 Add ability to add multiple times for restart.

--- src/main/java/com/teunjojo/SimpleAutoRestart.java
+++ src/main/java/com/teunjojo/SimpleAutoRestart.java
@@ -2,6 +2,8 @@ package com.teunjojo;
 
 import java.time.Duration;
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -20,40 +22,66 @@ public final class SimpleAutoRestart extends JavaPlugin {
         FileConfiguration config = this.getConfig();
         saveConfig();
 
-        String restartTime = (config.getString("restartTime"));
-
-        String[] timef = restartTime.split(":");
-        int hour = Integer.parseInt(timef[0]);
-        int minute = Integer.parseInt(timef[1]);
-
         int pluginId = 17760;
         Metrics metrics = new Metrics(this, pluginId);
 
-        ZonedDateTime now = ZonedDateTime.now();
-        ZonedDateTime nextRestart = now.withHour(hour).withMinute(minute).withSecond(0);
-        if (now.compareTo(nextRestart) > 0)
-        nextRestart = nextRestart.plusDays(1);
+        List<String> restartTimes;
+        Object restartTimeObject = config.get("restartTime");
 
-        Duration duration = Duration.between(now, nextRestart);
-        long initialDelay = duration.getSeconds();
-        WarnReboot("Restarting in 5 minutes", (int) initialDelay - 5*60, false);
-        WarnReboot("Restarting in 3 minutes", (int) initialDelay - 3*60, false);
-        WarnReboot("Restarting in 2 minutes", (int) initialDelay - 2*60, false);
-        WarnReboot("Restarting in 1 minute", (int) initialDelay - 1*60, false);
-        WarnReboot("Restarting in 3 seconds", (int) initialDelay - 3, false);
-        WarnReboot("Restarting in 2 seconds", (int) initialDelay - 2, false);
-        WarnReboot("Restarting in 1 second", (int) initialDelay - 1, false);
-        WarnReboot("Restarting now", (int) initialDelay, true);
+        if (restartTimeObject instanceof String) {
+            // Handle single string case
+            restartTimes = new ArrayList<>();
+            restartTimes.add((String) restartTimeObject);
+        } else if (restartTimeObject instanceof List) {
+            // Handle list case
+            restartTimes = (List<String>) restartTimeObject;
+        } else {
+            // Handle unexpected type or null
+            getLogger().warning("Invalid format for 'restartTime' in the configuration file.");
+            // Provide a default value or handle the situation accordingly
+            restartTimes = new ArrayList<>();
+        }
+
+        for (String restartTime : restartTimes) {
+            String[] timef = restartTime.split(":");
+            int hour = Integer.parseInt(timef[0]);
+            int minute = Integer.parseInt(timef[1]);
+
+            ZonedDateTime now = ZonedDateTime.now();
+            ZonedDateTime nextRestart = now.withHour(hour).withMinute(minute).withSecond(0);
+            if (now.compareTo(nextRestart) > 0)
+                nextRestart = nextRestart.plusDays(1);
+
+            Duration duration = Duration.between(now, nextRestart);
+            long initialDelay = duration.getSeconds();
+            setSchedule((int) initialDelay);
+            getLogger().info("Reboot set for: " + restartTime);
+        }
     }
 
     @Override
     public void onDisable() {
     }
 
-    public void WarnReboot(String message, int seconds, Boolean reboot) {
-        Timer timer;
-        timer = new Timer();
-        timer.schedule(new WarnTask(message, reboot), seconds * 1000);
+    public void setSchedule(int seconds) {
+        Timer timer = new Timer();
+        long in5Minutes = Math.max(0, (seconds - 5 * 60) * 1000);
+        long in3Minutes = Math.max(0, (seconds - 3 * 60) * 1000);
+        long in2Minutes = Math.max(0, (seconds - 2 * 60) * 1000);
+        long in1Minute = Math.max(0, (seconds - 1 * 60) * 1000);
+        long in3Second = Math.max(0, (seconds - 3) * 1000);
+        long in2Second = Math.max(0, (seconds - 2) * 1000);
+        long in1Second = Math.max(0, (seconds - 1) * 1000);
+
+        timer.schedule(new WarnTask("Restarting in 5 minutes", false), in5Minutes);
+        timer.schedule(new WarnTask("Restarting in 3 minutes", false), in3Minutes);
+        timer.schedule(new WarnTask("Restarting in 2 minutes", false), in2Minutes);
+        timer.schedule(new WarnTask("Restarting in 1 minute", false), in1Minute);
+        timer.schedule(new WarnTask("Restarting in 3 seconds", false), in3Second);
+        timer.schedule(new WarnTask("Restarting in 2 seconds", false), in2Second);
+        timer.schedule(new WarnTask("Restarting in 1 second", false), in1Second);
+
+        timer.schedule(new WarnTask("Restarting now", true), seconds * 1000);
     }
 
     class WarnTask extends TimerTask {
